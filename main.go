4e3e93d7b0637c64d9d9bca56fbd5e06bbd62663
mdaisuke <daisuke.mori9@gmail.com>
2017-08-25 09:37:54 +0900

    lexer

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/user"
+
+	"github.com/mdaisuke/monk/repl"
+)
+
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("hello %s!\n", user.Username)
+	repl.Start(os.Stdin, os.Stdout)
+}
